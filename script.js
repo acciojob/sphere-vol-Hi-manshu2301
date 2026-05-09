8a44e6868d869b98f117049fee02d4528e60b953
Hi-manshu2301 <104667314+Hi-manshu2301@users.noreply.github.com>
2026-05-09 10:27:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function volume_sphere() {
     //Write your code here
+	let radius = document.getElementById("radius").value;
+	radius = Number(radius);
+	if(isNaN(radius) || radius<0){
+		("Please enter a valid non-negative number");
+	}
+	let volume = 4/3 * Math.PI*radius*radius*radius;
+	volume = volume.toFixed(4)
+	document.getElementById("volume").value = volume;
   
 } 
 
